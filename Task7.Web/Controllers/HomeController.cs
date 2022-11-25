32c8c22e10a7a6eab6883cbea717771673634a99
SnEG1R <steamsnegir@gmail.com>
2022-11-25 18:42:21 +0300

    initial

---
+++ Task7.Web/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Task7.Web.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
