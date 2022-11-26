23518b32e351738513aa76580d9ae9478a1ff26d
SnEG1R <steamsnegir@gmail.com>
2022-11-26 19:53:00 +0300

    add game creation

---
+++ Task7.Application/Common/Constants/GameStatuses.cs
@@ -0,0 +1,9 @@
+namespace Task7.Application.Common.Constants;
+
+public class GameStatuses
+{
+    public const string Created = "Created";
+    public const string Launched = "Launched";
+    public const string Completed = "Completed";
+    public const string Interrupted = "Interrupted";
+}
--- Task7.Domain/Game.cs
+++ Task7.Domain/Game.cs
@@ -3,8 +3,8 @@ namespace Task7.Domain;
 public class Game
 {
     public long Id { get; set; }
-    public string ConnectionId { get; set; }
-    public string[] PlayingField { get; set; }
+    public Guid ConnectionId { get; set; }
+    public char[] PlayingField { get; set; }
     public string Status { get; set; }
 
     public List<Player> Players { get; set; }
