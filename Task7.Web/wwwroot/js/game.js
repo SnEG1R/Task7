18472c5843e4b2c15485414d52131bb8d9e657c4
SnEG1R <steamsnegir@gmail.com>
2022-11-30 21:47:13 +0300

    fix size chips

--- Task7.Web/wwwroot/js/game.js
+++ Task7.Web/wwwroot/js/game.js
@@ -1,7 +1,7 @@
-let cross = `<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+let cross = `<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-100 bi bi-x-lg" viewBox="0 0 16 16">
                 <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
             </svg>`;
-let zero = `<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-circle" viewBox="0 0 16 16">
+let zero = `<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-100 bi bi-circle" viewBox="0 0 16 16">
                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
             </svg>`;
 
